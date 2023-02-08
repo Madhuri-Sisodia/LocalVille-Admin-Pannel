02af88cee9b81872cff62bcf138f2a0c4a6928db
sneha-localville <sneha.agarwal@localville.in>
2023-02-08 17:51:20 +0530

    Ui improved of Categories

--- src/views/Category.js
+++ src/views/Category.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../assets/css/admin.css';
-import { Form, Radio, RadioGroup, } from "rsuite";
+import { Form, Radio, RadioGroup, Button, ButtonToolbar } from "rsuite";
 
 
 function Category() {
@@ -8,52 +8,58 @@ function Category() {
         <div className="AddAdminMainContainer">
             <div className="AdminContainer">
                 <Form fluid>
-                    <div className="InnnerContainerAdmin">
+                    {/* <div className="InnnerContainerAdmin"> */}
                         <Form.Group controlId="name-1">
-                            <Form.ControlLabel style={{ color: "#808080" }}>CATEGORY NAME</Form.ControlLabel>
+                            <Form.ControlLabel style={{ color: "#808080" , fontSize:"1rem"}}>Category name</Form.ControlLabel>
                             <Form.Control placeholder="Category Name" name="name" />
                         </Form.Group>
                         <Form.Group controlId="name-1">
-                            <Form.ControlLabel style={{ color: "#808080" }}>CATEGORY</Form.ControlLabel>
+                            <Form.ControlLabel style={{ color: "#808080", fontSize:"1rem" }}>Category</Form.ControlLabel>
                             <Form.Control placeholder="Category" name="name" />
                         </Form.Group>
+                    {/* </div> */}
+                    {/* <br /> */}
+                    <Form.ControlLabel style={{ color: "#808080", fontSize:"1rem"}}>Contain Sizes</Form.ControlLabel>
+
+                    <div className="InnnerContainer">
+                        <Form.Group controlId="radioList">
+                            <RadioGroup name="radioList">
+                                <Radio value="0">No</Radio>
+                                <Radio value="1">Yes</Radio>
+
+                            </RadioGroup>
+                        </Form.Group>
                     </div>
-                    <br/>
-                    <p>Contain Sizes</p>
-                    <div className="InnnerContainerAdmin">
-                    <Form.Group controlId="radioList">
-                        <RadioGroup name="radioList" inline>
-                            <Radio value="A">Item A</Radio>
-                            <Radio value="B">Item B</Radio>
-                            <Radio value="C">Item C</Radio>
-                            <Radio value="D">Item D</Radio>
-                        </RadioGroup>
+                    <br />
+                    <Form.ControlLabel style={{ color: "#808080", fontSize:"1rem"}}>Contain Colors</Form.ControlLabel>
+                    <div className="InnnerContainer">
+                        <Form.Group controlId="radioList">
+                            <RadioGroup name="radioList">
+                                <Radio value="0">No</Radio>
+                                <Radio value="1">Yes</Radio>
+
+                            </RadioGroup>
+                        </Form.Group>
+                    </div>
+                    <br />
+                    <Form.ControlLabel style={{ color: "#808080", fontSize:"1rem"}}>sub-Categories</Form.ControlLabel>
+                    <div className="InnnerContainer">
+
+                        <Form.Group controlId="radioList">
+                            <RadioGroup name="radioList">
+                                <Radio value="0">No</Radio>
+                                <Radio value="1">Yes</Radio>
+
+                            </RadioGroup>
+                        </Form.Group>
+
+                    </div>
+                    <Form.Group>
+                        <ButtonToolbar>
+                            <Button appearance="primary" type="submit" style={{ marginTop: "3rem", marginBottom: "0.5rem" }} block>Submit</Button>
+
+                        </ButtonToolbar>
                     </Form.Group>
-                    </div>
-                    <br/>
-                    <p>Contain Colors</p>
-                    <div className="InnnerContainerAdmin">
-                    <Form.Group controlId="radioList">
-                        <RadioGroup name="radioList" inline>
-                            <Radio value="A">Item A</Radio>
-                            <Radio value="B">Item B</Radio>
-                            <Radio value="C">Item C</Radio>
-                            <Radio value="D">Item D</Radio>
-                        </RadioGroup>
-                    </Form.Group>
-                    </div>
-                    <br/>
-                    
-                    <div className="InnnerContainerAdmin">
-                    <Form.Group controlId="radioList">
-                        <RadioGroup name="radioList" inline>
-                            <Radio value="A">Item A</Radio>
-                            <Radio value="B">Item B</Radio>
-                            <Radio value="C">Item C</Radio>
-                            <Radio value="D">Item D</Radio>
-                        </RadioGroup>
-                    </Form.Group>
-                    </div>
                 </Form>
             </div>
         </div>
